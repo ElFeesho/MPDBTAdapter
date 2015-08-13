639fed3b4c9268912f63533b33d4bbceb945e28f
Chris Sawczuk <gummybassist@gmail.com>
2015-08-13 19:02:03 +0000

    Bluetooth reading correctly

--- btserver.py
+++ btserver.py
@@ -2,15 +2,50 @@
 
 from bluetooth import *
 
-server_sock=BluetoothSocket( RFCOMM )
-server_sock.bind(("",PORT_ANY))
-server_sock.listen(1)
+class BTClient:
+	client_sock = None
+	def __init__(self, client_sock):
+		self.client_sock = client_sock
 
-port = server_sock.getsockname()[1]
+	def read(self):
+		buffer = ""
+		try:
+			while True:
+				data = self.client_sock.recv(1024)
+				if len(data) == 0:
+					break
+				buffer = buffer + data
+		except IOError:
+			print("Client disconnection?")
+			# Client disconnected?
+		return buffer
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+	def send(data):
+		self.client_sock.send(data)
 
-advertise_service(server_sock, "MPDBTAdapter",
-                  service_id = uuid,
-                  service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                  profiles = [ SERIAL_PORT_PROFILE ])
+class BTServer:
+	server_sock = None
+	channel = None
+
+	def __init__(self):
+		self.server_sock = BluetoothSocket( RFCOMM )
+		self.server_sock.bind(("",PORT_ANY))
+		self.channel = self.server_sock.getsockname()[1]
+	
+	def listen(self):
+		self.server_sock.listen(1)
+		print("Listening for bluetooth connections")
+
+	def advertiseServices(self):
+		uuid = "00001101-0000-1000-8000-00805F9B34FB"
+		advertise_service(self.server_sock, "MPDBTAdapter",
+         service_id = uuid,
+         service_classes = [ uuid, SERIAL_PORT_CLASS ],
+         profiles = [ SERIAL_PORT_PROFILE ])
+		print("Advertising services")
+
+	def acceptClient(self):
+		client_sock, client_info = self.server_sock.accept()
+
+		print("Accepted connection from ", client_info)
+		return BTClient(client_sock)
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python
 
 from socketdelegate import SocketConnection
+from btserver import BTServer
+from btserver import BTClient
 
 if __name__ == "__main__":
 	connection = SocketConnection("0.0.0.0", 6600)
 	print(connection.read())
-	connection.close()
-	pass	
+
+	btserver = BTServer()
+	btserver.listen()
+
+	btserver.advertiseServices()
+
+	while True:
+		btclient = btserver.acceptClient()
+
+		while True:
+			readData = btclient.read()
+			print("Read from bluetooth %s" % readData)
+			connection.send(readData)
+			response = connection.read()
+			print("Read from network: %s" % response)
+			btclient.send(response)
+
